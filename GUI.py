ef21bdb91dc5829aba55c23643f96ab84f567935
Charles Abe <abecharles5@gmail.com>
2021-11-09 17:01:44 +0800

    finish the input window

--- GUI.py
+++ GUI.py
@@ -17,7 +17,36 @@ class FCFS():
         sc.resizable(False, False)
         # Windows title
         sc.title(self.title)
-        # Windows Background Color
-        sc.config(background=self.background_color)
+        # Rectangle
+        my_canvas = Canvas(sc, width=720, height=480, bg=BG_COLOR, bd=0, highlightthickness=0)
+        my_canvas.pack()
+        my_canvas.create_rectangle(10, 468, 710, 465, fill=TEXT_BORDER_COLOR)
+        my_canvas.create_rectangle(10, 13, 710, 10, fill=TEXT_BORDER_COLOR)
+        # Label
+        algo_title = Label(sc, text=ALGO_NAME, font=("Play", 48, "bold"), fg=TEXT_BORDER_COLOR, bg=BG_COLOR)
+        algo_title.place(x=50, y=50)
+        # Entry for arrival time
+        arrival_time_label = Label(sc, text="Arrival Time: ", font=("Play", 14, "bold"), fg=TEXT_BORDER_COLOR, bg=BG_COLOR)
+        arrival_time_label.place(x=300, y=120)
+        arrival_time = Entry(sc, font=("Play", 24), bg=ENTRY_BG_COLOR, bd=5)
+        arrival_time.place(x=300, y=150)
+        # Entry for burst time
+        burst_time_label = Label(sc, text="Burst Time: ", font=("Play", 14, "bold"), fg=TEXT_BORDER_COLOR,
+                                   bg=BG_COLOR)
+        burst_time_label.place(x=300, y=250)
+        burst_time = Entry(sc, font=("Play", 24), bg=ENTRY_BG_COLOR, bd=5)
+        burst_time.place(x=300, y=275)
         # Function to initialize the window
         sc.mainloop()
+
+    def chart_window(self):
+        root = Tk()
+        # Windows size
+        root.geometry(self.window_size)
+        root.resizable(False, False)
+        # Windows Title
+        root.title(self.title)
+        # Windows Background Color
+        root.config(background=self.background_color)
+        # Function to initialize the window
+        root.mainloop()
