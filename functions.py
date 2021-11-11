9fc048f82fadad114c1dab3bdd2e54eebde3d2f5
Charles Abe <abecharles5@gmail.com>
2021-11-11 16:42:12 +0800

    add functions for fcfs algo and for making gantt chart

---
+++ functions.py
@@ -0,0 +1,11 @@
+
+
+def fcfs_sched(arrival_time, burst_time):
+    arrival_time = [int(i) for i in arrival_time.split()]
+    burst_time = [int(i) for i in burst_time.split()]
+
+    return arrival_time, burst_time
+
+
+def gantt_chart(arrival_time):
+    pass
